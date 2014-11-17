e501bc040bdab2e5128f6da4a5cfafbe90604092
Matthias Balke <matthias.balke@googlemail.com>
2014-11-17 14:22:54 +0100

    initial import

---
+++ clean.cmd
@@ -0,0 +1,20 @@
+del .project
+rmdir /S /Q .settings
+
+del company-parent\.project
+rmdir /S /Q company-parent\.settings
+
+del example\.project
+rmdir /S /Q example\.settings
+
+del example\qa-project\.checkstyle
+del example\qa-project\.pmd
+del example\qa-project\.pmdruleset.xml
+del example\qa-project\.fbIncludeFilterFile
+rmdir /S /Q example\qa-project\.settings
+
+
+rmdir /S /Q qa-config\.settings
+
+
+mvn clean eclipse:clean
